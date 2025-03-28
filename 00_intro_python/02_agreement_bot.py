07aa6f637e20158c8d9c9578e9d420a27fe1a918
ersa rani <161242905+ersa-rani@users.noreply.github.com>
2025-03-29 00:37:08 +0500

    Delete 00_intro_python directory

--- 00_intro_python/02_agreement_bot.py
+++
@@ -1,9 +0,0 @@
-def main():
-    # Prompt the user for their favorite animal
-    animal = input("What's your favorite animal? ")
-
-    # Print the response
-    print(f"My favorite animal is also {animal}!")
-
-if __name__ == '__main__':
-    main()
